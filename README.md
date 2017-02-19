0b0e449ec368e24b6845d917939f5a92c9bfe6f1
Tod E. Kurt <tod@todbot.com>
2017-02-18 22:02:09 -0800

    fixes for clarity

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ Testing Playstation DualShock4 controller with node-hid
 
 Getting data from DS4 is easy, there are many packages that do it.
 
-Sending data to set rumble and LED appears to be hard.
-This demo attempts to do that, mostly emulating what is found in
+Sending data to set rumble and LED via BT appears to be harder than on USB.
+Mostly this seems to be because you need to add CRC32 calc.
+
+This project demo attempts to do that, mostly emulating what is found in
 the `hid-sony` driver for Linux:
 https://github.com/torvalds/linux/blob/master/drivers/hid/hid-sony.c#L2048
 
@@ -15,8 +17,12 @@ To try it out:
 2. Check out this repo
 3. `npm install`
 4. `npm start`
-5. Press Square button to start rumbling
+5. Watch controller data
+6. Watch pretty colors change on controller
 
 
 Issues:
-- Currently the controller stops responding after sending rumble command
+- Once the controller is updated with an Output Report to update LED & rumble,
+it stops streaming Input Reports for reportId 1.  Unclear how to re-enable.
+
+
--- node-hid-ds4-test.js
+++ node-hid-ds4-test.js
@@ -1,8 +1,12 @@
 //
-// node-hid-ds4-test.js -- Use node-hid to receive movement events
+// node-hid-ds4-test.js -- Demonstrate using node-hid to receive data
 //                         from DualShock4 DS4 controlller and
 //                         send rumble and LED to controller
 //
+// Note: DS4 controller stops emitting Input Report #1
+//       events after Output Report is sent.  
+//
+// 2017 - Tod E. Kurt / todbot.com 
 //
 
 var crc = require('crc');
@@ -11,7 +15,13 @@ var HID = require('node-hid');
 
 var device = new HID.HID(1356, 1476);
 
-//var tmpbuf = device.getFeatureReport(0x02, 37);
+var rumbleL = 0;
+var rumbleR = 0;
+var ledR = 255;
+var ledG = 0;
+var ledB = 255;
+
+
 
 //
 // Set rumble and LED
@@ -20,16 +30,16 @@ var device = new HID.HID(1356, 1476);
 //
 function setRumbleLed(rumbleL, rumbleR, ledR,ledG,ledB)
 {
-    var buf = [
-        0x11, // reportId
-        0xc0, 0, 0x0f,  0,  0,  rumbleL,  rumbleR, ledR, ledG, ledB,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0 ]; // note: buf is 4-bytes short, for crc32
+
+    var buf = Array(78-4).fill(0); // 4 bytes short for CRC32 calc below
+    buf[0]  = 0x11; // reportId
+    buf[1]  = 0x80;
+    buf[3]  = 0x0f;
+    buf[6]  = rumbleL;
+    buf[7]  = rumbleR;
+    buf[8]  = ledR;
+    buf[9]  = ledG;
+    buf[10] = ledB;
     
     // calculate crc32, add those 4 bytes to end of buf array
     var tmpbuf = buf.slice(0); // clone
@@ -41,21 +51,35 @@ function setRumbleLed(rumbleL, rumbleR, ledR,ledG,ledB)
     buf[77] = (crc32 & 0xff000000) >> 24;
     // end crc32 calc
     
-    console.log("buf:","len:",buf.length, "vals:",buf.join(','));
+    console.log("output buf:","len:",buf.length, "vals:",buf.join(','));
     device.write(buf);
+
 }
 
-device.gotData = function (err, data) {
+// send an update to change rumble & LED ever 500 msec
+var updateController = function() {
+    setRumbleLed( rumbleL, rumbleR, ledR, ledG, ledB);
+    ledR += 50; // make up some random colors for next time
+    ledG += 40;
+    ledG += 90;
+    setTimeout( updateController, 500);
+}
+
+setTimeout( updateController, 1500 );
+
+
+var getControllerData = function( data ) {
     console.log('got ds4 data', data);
     // data[5] contains square button state at 0x10 bit
-    if( (data[5] & 0x10) ) { 
+    if( (data[5] & 0x10) ) { // square pressed!
         console.log("square pressed!");
-        setRumbleLed(120,120, 255,0,255); // half rumbles + purple
-        console.log("rumble done");
+        rumbleL = 255;
     }
-    this.read(this.gotData.bind(this));
+    else {
+        rumbleL = 0; 
+    }
 };
 
-device.read(device.gotData.bind(device));
-
+device.on('data', getControllerData);
+          
 console.log("press Square to turn on rumble");
